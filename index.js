5a5e9e7d8e2f307773c0edc59f14e0a4c788aafb
ayuwn <ayuwidianingsih@students.unnes.ac.id>
2022-12-13 15:07:56 +0700

    done

---
+++ index.js
@@ -0,0 +1,56 @@
+let weather = {
+    apiKey: "dcb4ff44f9275447af1997c1a8d43d6a",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("Cuaca tidak ditemukan.");
+            throw new Error("Cuaca tidak ditemukan.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = name;
+      document.querySelector(".city-name").innerText = name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".condition").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".temperature").innerText = temp + "°C";
+      document.querySelector(".wind").innerText = speed + " km/h";
+      document.querySelector(".wind-speed").innerText = speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Jakarta");
+  
